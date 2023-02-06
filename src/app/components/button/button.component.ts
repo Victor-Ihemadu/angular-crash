ad32ee52c074c20455d182ad561b980d2395c2b8
Victor Izuchukwu Ihemadu <bigDev@Victors-MacBook-Air.local>
2023-02-06 13:45:42 +0100

    task component created

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./button.component.css']
 })
 export class ButtonComponent {
+  @Input() text: string | undefined;
+  @Input() color: string | undefined;
+  @Output() btnClick = new EventEmitter
 
+  onClick() {
+    this.btnClick.emit();
+  }
 }
