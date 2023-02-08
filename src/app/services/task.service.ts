7fc787b72e6de5baa293371d41465e615a4a888a
Victor Izuchukwu Ihemadu <bigDev@Victors-MacBook-Air.local>
2023-02-08 10:51:12 +0100

    addTask component created

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,19 +1,33 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {Observable, of} from 'rxjs'
+import { Observable } from 'rxjs'
 import { Task } from '../Task'
-import { TASKS } from '../mock-tasks';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  })
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
-  private apiURL = 'http://localhost:500/tasks'
+  private apiURL = 'http://localhost:5000/tasks'
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   getTasks(): Observable<Task[]> {
-    const taks = of(TASKS);
-    return taks;
+    return this.http.get<Task[]>(this.apiURL)
+  }
+
+  deleteTask(task: Task): Observable<Task> {
+    const url = `${this.apiURL}/${task.id}`
+    return this.http.delete<Task>(url)
+  }
+
+  updateTaskReminder(task: Task): Observable<Task> {
+    const url = `${this.apiURL}/${task.id}`
+    return this.http.put<Task>(url, task, httpOptions)
   }
 }
